327ee9a613c44d6ad5131effd1b6b16eb06049e2
codeamac19 <codeamac19@gmail.com>
2017-06-29 17:58:08 -0500

    has many through association developed

--- db/migrate/20170629164545_create_posts.rb
+++ db/migrate/20170629164545_create_posts.rb
@@ -1,4 +1,10 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
+    create_table :posts do |t|
+      t.string :autor
+      t.text :body
+      t.string :title
+      t.timestamps
+    end
   end
 end
--- db/migrate/20170629165750_create_post_tags.rb
+++ db/migrate/20170629165750_create_post_tags.rb
@@ -1,4 +1,11 @@
-class CreatePostTags < ActiveRecord::Migration
+class CreatePostTags < ActiveRecord::Migration[5.0]
   def change
+    create_table :post_tags do |t|
+      t.belongs_to :tags
+      t.belongs_to :posts
+      # t.integer :id_post
+      # t.integer :id_tag
+      t.timestamps
+    end
   end
 end
