65bffa6133345f21e90be4b6cd1cf953efcfa6da
codeamac19 <codeamac19@gmail.com>
2017-06-29 21:34:48 -0500

    Dessarrollando metodo para busqueda de post

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,8 +1,7 @@
+# require 'sinatra'
+# set :show_exceptions, false
 # Todas las rutas relacionadas con Post irán en el controlador posts
 =begin
-
-	-Mostrar un post determinado.
-
 
 	-Editar un post existente.
 	-Eliminar un post existente.
@@ -11,12 +10,35 @@ def show_all_post
 	Post.all
 end
 
+# error do
+# 	@looked_post = @looked_post.errors.full_messages
+# end
+
+def look_for_post(looked_post)
+	looked_post_integer = looked_post.to_i
+	case
+	when looked_post_integer != 0
+		@looked_post = Post.where(id: looked_post)
+		if @looked_post.blank?
+			#errors[:base] << "Ninguno con ese id" : false;
+			p @errors = "no registros"
+			erb :index
+		end
+	else
+		@looked_post = Post.where(autor: looked_post)
+		@looked_post.empty?
+	end
+
+end
+
 # -Mostrar todos los posts.
-post "/show_all_post" do
-	p "<"*50
-	p  @all_posts = show_all_post
-	p "<"*50
+get "/show_all_post" do
+	@all_posts = show_all_post
 	erb :index
+end
+#-Mostrar un post determinado.
+post "/show_specific_post" do
+	look_for_post(params[:looked_post])
 end
 
 # -Crear un post nuevo.
