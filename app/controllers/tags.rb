edc11b56e530fa6a263b9d16a2ac5e473d2d3b3a
codeamac19 <codeamac19@gmail.com>
2017-07-05 17:51:45 -0500

    deveolpment of post search throw tag

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,43 +1,60 @@
 #Todas las rutas relacionadas con Tag irán en el controlador tags.
-#Mostrar todos los posts con cierto Tag.
-#Las Tags serán creadas al momento que se creen los Posts.
 
+#Las Tags serán creadas al momento que se creen los Posts.
 after "/postear" do
 
 		unless params[:post][:tags].empty?
-
+			# access to the values of the hass params throw the keys
 		  input_in_the_form = params[:post][:tags]
+			# separate the input throw a regex with comas, ignoring black spaces and put in a Ary
 			ary_whit_input = input_in_the_form.split (/\s*,\s*/)
+			# itarate throw the ary
 			ary_whit_input.each do |tag_name|
-
+				# find if the tag already exists
 				tag = Tag.find_by(name: tag_name)
+				# if not already exists
 				if tag.blank?
+					# create a new tag
 					tag = Tag.create!(name: tag_name)
 				end
+				# retrieve the last post
 				last_post = Post.last
-				new_tag = PostTag.create!(tags_id: tag.id, posts_id: last_post.id)
-				p "s"*50
-				p new_tag
-				p "s"*50
+				# create the Join relation with tha tag id and ths post id
+				PostTag.create!(tags_id: tag.id, posts_id: last_post.id)
 			end
 
 		end
 end
 
-		# unless params[:tag].empty?
-		# 	tag = Tag.find_by(name: params[:tag])
-		# 	if tag.blank?
-		# 		tag = Tag.new(name: params[:tag])
-		# 		tag.save
-		# 		tag =  Tag.last
-		# 		post = Post.last
-		# 		p PostTag.create!(tags_id: tag.id, posts_id: post.id)
-		# 	else
-		# 		p "<"*50
-		# 		p tag
-		# 		post = Post.last
-		# 		p PostTag.create!(tags_id: tag.id, posts_id: post.id)
-		# 		p "<"*50
-		# 	end
-		# 	p PostTag.where(name: params[:tag])
-		# end
+#Mostrar todos los posts con cierto Tag.
+post "/show_tag" do
+
+
+	 unless params[:tags][:searched_tag].empty?
+		 # access to the values of the hass params throw the keys
+		 input_in_the_form = params[:tags][:searched_tag]
+		 # separate the input throw a regex with comas, ignoring black spaces and put in a Ary
+		 ary_whit_input = input_in_the_form.split (/\s*,\s*/)
+		 # itarate throw the ary
+		 ary_whit_input.uniq.each do |tag_name|
+			 # find if the tag already exists
+
+
+			 p tags = Tag.where(name: tag_name)
+			 # if not already exists
+			 if tags.blank?
+				 # create a new tag
+				 @errors = "No existe tag:  #{tag_name}"
+			 else
+				 tags.each do |tag|
+					 	tag
+				 end
+			 end
+
+
+		 end
+
+	 end
+
+	erb :index
+end
